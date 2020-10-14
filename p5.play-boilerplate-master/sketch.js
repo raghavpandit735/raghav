f7396bf40909f0009d25acd64ecc97e8c2155993
raghavpandit735 <72440001+raghavpandit735@users.noreply.github.com>
2020-10-14 19:44:50 +0530

    Add files via upload

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,25 +1,38 @@
-var car, ball, speed, weight;
+var wall, thickness, bullet, speed, weight, Ibullet, Iwall;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-
-  speed = random(55, 90);
-  weight = random(400, 1500);
-  car = createSprite(50, 200, 50, 50);
-  car.velocityX = speed;
-  wall = createSprite(1500, 200, 60, height/2);
-  if(wall.x-car.x<(car.width+wall.width)/2){
-  car.velocityX = 0;
-   var deformation = 0.5*weight*speed*speed/22509;
-   if (deformation>180){
-       car.shapeColor =color(0, 255, 0,);
-   }
-    }
-
+  createCanvas(1600, 400);
+  thickness = random(23, 83);
+  wall = createSprite(1200, 200, thickness, height/2)
+  wall.shapeColor = ("blue");
+  speed = random(223, 321);
+  weight = random(30, 52);
+bullet = createSprite(600, 200, 70, 10);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black"); 
+  bullet.velocityX = speed;
   drawSprites();
+  if(hascollided(bullet, wall))
+  {
+    bullet.velocityX=0
+    var damage=0.5*weight*speed*speed/(thickness* thickness *thickness);
+
+  
+    if(damage>10){
+      wall.shapecolor=color(255,0,);
+
+    }
+     wall.shapeColor=color(0,255,0);
+  }
 }
 
+function hascollided(Ibullet, Iwall) {
+bulletRightEdghe=Ibullet.x +Ibullet.width;
+wallLeftEdge=Iwall.x;
+if(bulletRightEdge>=wallLeftedge){
+  return true
+}
+return false;
+}
